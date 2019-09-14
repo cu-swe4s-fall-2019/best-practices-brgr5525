0cced6af7433376bba628ee0f255030d789840ea
Brianna Griffin <briannagriffin@Keiths-MacBook-Pro.local>
2019-09-13 22:36:22 -0600

    added tests and echos to show exception handling

--- basics_test.sh
+++ basics_test.sh
@@ -2,17 +2,24 @@
 
 chmod -x basics_test.sh
 
+echo ...running pycodestyle on style.py...
 pycodestyle style.py
 
+echo ...running pycodestyle on get_column_stats.py...
 pycodestyle get_column_stats.py
 
+echo ...running get_column_stats.py -h...
 python get_column_stats.py -h
 
+echo ...running get_column_stats with wrong_file.txt...
+python get_column_stats.py --file_name wrong_file.txt --col_num 2
+
+echo ...running get_column_stats using original tests...
 (for i in `seq 1 100`; do
     echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
 done )> data.txt
 
-python get_column_stats.py --file_name data.txt --col_num 2
+ python get_column_stats.py --file_name data.txt --col_num 2
 
 
 V=1
