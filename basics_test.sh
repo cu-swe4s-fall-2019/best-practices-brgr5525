3bed92a4d3d1a30c00eea0a7e8c507cb3b09f6c7
briannagriffin <briannagriffin@Keiths-MacBook-Pro.local>
2019-09-13 22:06:17 -0600

    added additional test and updated calls to work with argparse

--- basics_test.sh
+++ basics_test.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
+
+chmod -x basics_test.sh
+
 pycodestyle style.py
 
 pycodestyle get_column_stats.py
 
-(for i in `seq 1 100`; do 
+python get_column_stats.py -h
+
+(for i in `seq 1 100`; do
     echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
 done )> data.txt
 
-python get_column_stats.py data.txt 2
+python get_column_stats.py --file_name data.txt --col_num 2
 
 
 V=1
-(for i in `seq 1 100`; do 
+(for i in `seq 1 100`; do
     echo -e "$V\t$V\t$V\t$V\t$V";
 done )> data.txt
 
-python get_column_stats.py data.txt 2
+python get_column_stats.py --file_name data.txt --col_num 2
